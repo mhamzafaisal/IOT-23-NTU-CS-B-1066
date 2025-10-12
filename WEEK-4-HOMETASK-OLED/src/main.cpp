e638e889bfde8a0cd5d11c24a98985f88dc88605
hamza <mhamzafaisal2@gmail.com>
2025-10-12 11:56:37 +0500

    i

--- WEEK-4-HOMETASK-OLED/src/main.cpp
+++ WEEK-4-HOMETASK-OLED/src/main.cpp
@@ -2,7 +2,6 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-// ---- OLED setup ----
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_ADDR 0x3C
@@ -19,20 +18,22 @@ void setup() {
   display.clearDisplay();
 
   // ---- Text and Box ----
-  display.setTextSize(1);               // Normal text size
-  display.setTextColor(SSD1306_WHITE);  // White text
-  display.setCursor(25, 25);            // Start position for text
+  display.setTextSize(1);              
+  display.setTextColor(SSD1306_WHITE);
 
-  display.println("Hamza");
-  display.println("1066");
+  // Draw rectangle first (centered box)
+  display.drawRect(10, 10, 108, 44, SSD1306_WHITE);
 
-  // Draw rectangle around the text
-  display.drawRect(15, 15, 100, 40, SSD1306_WHITE);
+  // Place text neatly inside the box
+  display.setCursor(25, 22);
+  display.println("M. HAMZA");
+  display.setCursor(25, 34);  // Manual control of next line
+  display.println("23-NTU-CS-1066");
 
   // Display all on OLED
   display.display();
 }
 
 void loop() {
-  // Nothing to do repeatedly
+  // Nothing here
 }
